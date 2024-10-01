a36535890e2866fc925487336c6c2583613c1022
emi <your@email.com>
2024-10-01 17:38:18 -0300

    asdasd

--- src/main/java/com/retrofighters/arcade/handlers/EmulatorHandler.java
+++ src/main/java/com/retrofighters/arcade/handlers/EmulatorHandler.java
@@ -21,26 +21,34 @@ import java.io.InputStreamReader;
 public class EmulatorHandler {
     private final String _retroArchPath = "C:\\RetroArch-Win64\\retroarch.exe ";
     private final String _corePath = "C:\\RetroArch-Win64\\cores\\stella_libretro.dll "; // atari stella emulator
-    private final String _testRomPath = "C:\\Users\\axela\\Documents\\NetBeansProjects\\proyectoArcade\\src\\main\\java\\com\\retrofighters\\arcade\\assets\\ROMs\\spaceInvadersAtari.a26 "; // space invaders for atari
+    //private final String _testRomPath = "\"C:\\Users\\56125894\\Desktop\\proyecto arcade\\proyecto-arcade\\src\\main\\java\\com\\retrofighters\\arcade\\assets\\ROMs\\atari\\spaceInvadersAtari.a26\" "; // space invaders for atari
+    private final String _testRomPath = "C:\\Users\\56125894\\Desktop\\spaceInvadersAtari.a26"; // space invaders for atari
     
     // initializes the emulator instance
     public boolean initializeEmulator(Emulator pEmulator, Game pGame) throws IOException, InterruptedException{
-        ProcessBuilder processBuilder = new ProcessBuilder(
+        /*ProcessBuilder processBuilder = new ProcessBuilder(
             _retroArchPath,
             "--libretro=", _corePath,
             _testRomPath
-        );
-        processBuilder.redirectErrorStream(true);
+        );*/
+        String comando[] = {_retroArchPath,
+            "-L", _corePath,
+            _testRomPath}; 
+        String comando2 = _retroArchPath + " -L " + _corePath + " " + _testRomPath;
+        //processBuilder.redirectErrorStream(true);
+        Process proceso;
         try {
-            Process process = processBuilder.start();
-            try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+            //Process process = processBuilder.start();
+            proceso = new ProcessBuilder(comando).start();
+            proceso.waitFor();
+            /*try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
                 String line;
                 while ((line = reader.readLine()) != null) {
                  System.out.println(line); // Print output to console
                 }
             }
             int exitCode = process.waitFor();
-            System.out.println("Process finished with code: " + exitCode);
+            System.out.println("Process finished with code: " + exitCode);*/
         } catch (IOException | InterruptedException ex) {
             throw ex;
         }
