b49ab1e3a18ae790c2eaf1022679a68ccb71ffb5
retrofighters <retrofightersarcade@gmail.com>
2024-10-02 15:22:44 -0300

    asdads

--- src/main/java/com/retrofighters/arcade/handlers/EmulatorHandler.java
+++ src/main/java/com/retrofighters/arcade/handlers/EmulatorHandler.java
@@ -22,7 +22,7 @@ public class EmulatorHandler {
     private final String _retroArchPath = "C:\\RetroArch-Win64\\retroarch.exe ";
     private final String _corePath = "C:\\RetroArch-Win64\\cores\\stella_libretro.dll "; // atari stella emulator
     //private final String _testRomPath = "\"C:\\Users\\56125894\\Desktop\\proyecto arcade\\proyecto-arcade\\src\\main\\java\\com\\retrofighters\\arcade\\assets\\ROMs\\atari\\spaceInvadersAtari.a26\" "; // space invaders for atari
-    private final String _testRomPath = "C:\\Users\\56125894\\Desktop\\spaceInvadersAtari.a26"; // space invaders for atari
+    private final String _testRomPath = "C:\\Users\\RetroFighters\\Desktop\\Repo\\proyecto-arcade\\src\\main\\java\\com\\retrofighters\\arcade\\assets\\ROMs\\atari\\spaceInvadersAtari.a26"; // space invaders for atari
     
     // initializes the emulator instance
     public boolean initializeEmulator(Emulator pEmulator, Game pGame) throws IOException, InterruptedException{
